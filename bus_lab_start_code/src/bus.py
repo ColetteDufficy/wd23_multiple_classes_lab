69797806f08a908ba30156ed0ab739376f8aed21
colette dufficy <c.dufficy@gmail.com>
2022-03-23 14:41:59 +0000

    MVP completed

--- bus_lab_start_code/src/bus.py
+++ bus_lab_start_code/src/bus.py
@@ -1,7 +1,27 @@
 class Bus:
-    def __init__(self, route_num, destination):
-        self.route_num = route_num
+    def __init__(self, route_number, destination):
+        self.route_number = route_number
         self.destination = destination
+        self.passengers = []
         
-    def drive_noise(self):
+    def drive(self):
         return "Brum brum"
+    
+    def passenger_count(self):
+        return len(self.passengers)
+    
+    def pick_up(self, person):
+        self.passengers.append(person)
+        
+    def drop_off(self, person):
+        self.passengers.remove(person)
+        
+    def empty(self):
+        self.passengers.clear()
+
+    def pick_up_from_stop(self, person):
+        self.pick_up(person)
+        self.pick_up(person)
+        return len(self.passengers)
+        
+    
--- bus_lab_start_code/src/person.py
+++ bus_lab_start_code/src/person.py
@@ -2,7 +2,6 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.passengers = []
         
     def num_of_passengers(self):
         return len(self.passengers)
